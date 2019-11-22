9bfe8e875c0020e7caaf8181f1855c0582f45494
Sergio del Amo <sergio.delamo@softamo.com>
2019-11-22 07:14:17 +0100

    remove unused imports

--- complete/src/test/groovy/example/micronaut/BasicAuthClientSpec.groovy
+++ complete/src/test/groovy/example/micronaut/BasicAuthClientSpec.groovy
@@ -1,13 +1,6 @@
 package example.micronaut
 
 import io.micronaut.context.ApplicationContext
-import io.micronaut.http.HttpHeaders
-import io.micronaut.http.HttpRequest
-import io.micronaut.http.HttpResponse
-import io.micronaut.http.HttpStatus
-import io.micronaut.http.MutableHttpMessage
-import io.micronaut.http.client.RxHttpClient
-import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.runtime.server.EmbeddedServer
 import spock.lang.AutoCleanup
 import spock.lang.Shared
