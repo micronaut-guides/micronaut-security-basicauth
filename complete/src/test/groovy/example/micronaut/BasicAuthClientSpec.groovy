0252c6ffc3c2f13983eb0adc0e353d58616cc4f0
Iván López <lopezi@objectcomputing.com>
2019-11-25 11:38:21 +0100

    Use `@MicronautTest`

--- complete/src/test/groovy/example/micronaut/BasicAuthClientSpec.groovy
+++ complete/src/test/groovy/example/micronaut/BasicAuthClientSpec.groovy
@@ -1,27 +1,20 @@
 package example.micronaut
 
-import io.micronaut.context.ApplicationContext
-import io.micronaut.runtime.server.EmbeddedServer
-import spock.lang.AutoCleanup
-import spock.lang.Shared
+import io.micronaut.test.annotation.MicronautTest
 import spock.lang.Specification
 
+import javax.inject.Inject
+
+@MicronautTest // <1>
 class BasicAuthClientSpec extends Specification {
 
-    @Shared
-    @AutoCleanup // <1>
-    EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer) // <2>
+    @Inject
+    AppClient appClient // <2>
 
     def "Verify HTTP Basic Auth works"() {
         when:
-        AppClient appClient = embeddedServer.applicationContext.getBean(AppClient) // <3>
-
-        then:
-        noExceptionThrown() // <4>
-
-        when:
         String credsEncoded = "sherlock:password".bytes.encodeBase64().toString()
-        String rsp = appClient.home("Basic ${credsEncoded}") // <5>
+        String rsp = appClient.home("Basic ${credsEncoded}") // <3>
 
         then:
         rsp == 'sherlock'
